3807134fa537d74575098d9b1a4fc08920bf82c9
Mintu2322 <mintunagar1023@gmail.com>
2022-11-25 18:17:29 +0530

    Data-tpyes

---
+++ Data-types/index.js
@@ -0,0 +1,63 @@
+// String Data Type
+ const str1 = "Hello, This is Mintu Nagar";
+ const str2 = "Learn Data Type";
+
+ console.log("concat Using (+)", (str1 + ' ' + str2));
+ console.log("concat Using Function:" , (str1.concat(str2)));
+
+ var x = "Welcome to etutorialspoint";
+console.log(x);
+
+console.log('John\'s new shirt.')
+console.log("\"Hey, John!\", how are you.")
+
+
+// Number
+const a = 10;
+const b = 23;
+const c = 1232;
+console.log(a); 
+console.log(b); 
+console.log(c); 
+
+
+// boolean
+if(NaN){
+}else{
+   console.log("false");
+}
+if(undefined){
+}else{
+   console.log("false");
+}
+if(NaN){
+}else{
+   console.log("false");
+}
+
+// object
+
+const employee = {
+    name: "Soy",
+    age: "40",
+    department: "Software"
+   };
+   console.log(employee.name);
+   console.log(employee.age);
+   console.log(employee.department);
+
+//array
+var arr1 = ['apple', 'banana', 'pear', 'orange'];
+
+for (var i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+}
+
+
+//Function in Node.s
+
+ function Messsage(name) {
+    console.log("Hello "+name);
+   }
+   
+   Messsage("World");
